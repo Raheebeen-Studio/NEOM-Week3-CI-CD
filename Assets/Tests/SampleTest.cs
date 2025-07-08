6eb09591202bc67552453124690b17b099bb9cfb
Moe-Baker <moe4baker@gmail.com>
2025-07-08 10:29:50 +0300

    Add Sample Tests

---
+++ Assets/Tests/SampleTest.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+
+using NUnit.Framework;
+
+using UnityEngine;
+using UnityEngine.TestTools;
+
+public class SampleTest
+{
+    [Test]
+    public void TestEquals()
+    {
+        Assert.AreEqual(1, 1);
+    }
+}
